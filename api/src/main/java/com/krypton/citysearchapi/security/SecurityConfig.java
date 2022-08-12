3316fe04e1ac32244de6b30662b330c34f6ce15c
Akshay Joy <akshayjoyhmgroup@outlook.com>
2022-08-13 04:42:42 +0530

    fixed issue

--- api/src/main/java/com/krypton/citysearchapi/security/SecurityConfig.java
+++ api/src/main/java/com/krypton/citysearchapi/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers(HttpMethod.GET, "/api/v1/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll() // GET requests don't need auth
+                .mvcMatchers(HttpMethod.GET, "/api/v1/**", "/swagger","/swagger-ui/**", "/v3/api-docs/**").permitAll() // GET requests don't need auth
                 .anyRequest()
                 .authenticated()
                 .and()
